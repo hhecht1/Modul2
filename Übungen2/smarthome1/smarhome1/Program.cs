b1cbdc7e37aa24259a0af7c0bf0e4b4c84746391
Helge_ <helgehecht92@gmail.com>
2026-03-17 13:49:45 +0100

    Smarthome!

---
+++ Übungen2/smarthome1/smarhome1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Dynamic;
+
+
+public class Geräte
+{
+    public required string ModelName { get; init; }
+    
+    }
+public class SmartLampe :Geräte
+{
+    public int Helligkeit {get;private set;}
+    public List<string> Farbpalette {get;} = new();
+
+    public SmartLampe(string name ): base () {}
+
+    void SetzeFarben(params string[] farben)
+    {
+        Farbpalette.AddRange(farben);
+        Console.WriteLine($"{farben.Length} Farben wurden hinzugefügt.");
+    }
+
+    public bool HelligkeitSteuerung(int wert, out string status)
+    {
+        if(wert >= 0 && wert <=100)
+        {
+            Helligkeit = wert;
+            status = "Helligkeit auf {Helligkeit}% eingestellt.";
+            return true;
+        }
+        status = "Ungültiger Wert. Bitte zwischen 0 und 100 eingeben.";
+        return false;
+    }
+
+
+
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        SmartLampe lampe = new SmartLampe("Philips Hue");
+        lampe.SetzeFarben("Rot", "Grün", "Blau");
+        
+        if(lampe.HelligkeitSteuerung(75, out string status))
+        {
+            Console.WriteLine(status);
+        }
+        else
+        {
+            Console.WriteLine(status);
+        }
+    }
+}
