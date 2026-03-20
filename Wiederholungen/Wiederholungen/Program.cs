999662ed059081dde272547918f694097bbfedaf
Helge_ <helgehecht92@gmail.com>
2026-03-20 09:33:38 +0100

    Wiederholungen

---
+++ Wiederholungen/Wiederholungen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Wiederholungen
+{
+    public enum Gerätetyp
+    {
+        Licht,Heizung,Sicherheit,Unterhaltung
+    }
+
+    public struct StatusInfo
+    {
+        public double Wert;
+        public string Einheit;
+    }
+
+    public interface Controller
+    {
+        public void Einschalten();
+        public void Ausschalten();
+
+    }
+    public abstract class SmartGerät : Controller
+    {
+        public string Name {get;set;}
+        public Gerätetyp Typ {get;set;}
+    }
+}
