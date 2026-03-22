91f3733910d58582c7f38ce5d34ec497fda743c4
Helge_ <helgehecht92@gmail.com>
2026-03-22 11:30:11 +0100

    Smarthomeübung

---
+++ Smarthome3.0/Smarthome3/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Smarthome3
+{
+    public enum Gerätetyp
+    {
+        Licht,Heizung,Sicherheit,Energie
+    }
+
+    public struct StatusInfo
+    {
+        public double Wert;
+        public string Einheit;
+        public StatusInfo(double wert,string einheit)
+        {
+            Wert = wert;
+            Einheit = einheit;
+
+        }
+        
+    }
+
+    public interface Controller
+    {
+        public void TurnOn();
+        public void TurnOff();
+    }
+    public abstract class SmartGerät : Controller
+    {
+        public string Name {get;set;}
+        public Gerätetyp Typ {get;set;}
+
+        public abstract string GetStatus();
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Gerät: {Name} und {Typ} | Status: {GetStatus()}");
+        }
+
+        public abstract void TurnOn();
+        public abstract void TurnOff();
+    }
+
+    public class SmartLamp :SmartGerät
+    {
+        public bool iSoN {get; private set;}
+
+        public int Helligkeit {get; set;}
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Die Lampe ist jetzt an.");
+            iSoN = true;    
+        }
+        public override void TurnOff()
+        {
+            Console.WriteLine("Die Lampe ist jetzt aus.");
+            iSoN = false;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+
+        public override string GetStatus() => iSoN ? $"An ({Helligkeit})" : "Aus";
+
+        public void Dimmen(int wert )=> Helligkeit = wert;
+
+    }
+
+    public class SmartThermostat : SmartGerät
+    {
+        public bool heatingIsOn {get; private set;}
+
+        public int Temperature {get; set;}
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Die Heizung ist jetzt an.");
+            heatingIsOn = true;
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Die Heizung ist jetzt aus.");
+            heatingIsOn = false;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+
+        public override string GetStatus() => heatingIsOn ? $"An ({Temperature}°C)" : "Aus";
+    }
+
+    public class SmartSecurity : SmartGerät
+    {
+        public bool SecurityIsOn {get; private set;}
+        public int CameraCount {get; set;}
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Die Sicherheit ist jetzt an.");
+            SecurityIsOn = true;
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Die Sicherheit ist jetzt aus.");
+            SecurityIsOn = false;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+        public override string GetStatus() => SecurityIsOn ? $"Im Moment laufen  ({CameraCount} Kameras)" : "Aus";
+
+       
+    }
+
+    public class Smartenergy : SmartGerät
+    {
+        public bool EnergyIsOn {get; private set;}
+        public int EnergyUsage {get; set;}
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Die Energie ist jetzt an.");
+            EnergyIsOn = true;
+        }
+        public override void TurnOff()
+        {
+            Console.WriteLine("Die Energie ist jetzt aus.");
+            EnergyIsOn = false;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+         public override string GetStatus() => EnergyIsOn ? $"Energiegewinnung ({EnergyUsage} kWh)" : "Aus";
+    }
+}
